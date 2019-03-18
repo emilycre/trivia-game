156f012efc75d5e9e7decc54cc1b06de73a29b92
Cosmo Kent <cosmolightfoot@gmail.com>
2019-03-18 10:50:30 -0700

    TDD user profile header

--- test/header-template.test.js
+++ test/header-template.test.js
@@ -4,6 +4,38 @@ QUnit.module('header');
 
 import { makeHeaderTemplate } from '../src/load-header.js';
 
+function makeProfile(user) {
+    const template = document.createElement('template');
+    template.innerHTML = `
+        <div>
+            <img src="${user.photoURL}" id="user-image">
+            <span id="user-name">${user.displayName}</span>
+            <button id="sign-out">Sign Out</button>
+        </div>
+    `;
+
+    return template.content;
+}
+
+test('user profile template', function(assert) {
+    const expected = `
+        <div>
+            <img src="./assets/auth.jpeg" id="user-image">
+            <span id="user-name">Banana Man</span>
+            <button id="sign-out">Sign Out</button>
+        </div>
+    `;
+
+    const user = {
+        displayName: 'Banana Man',
+        photoURL: './assets/auth.jpeg'
+    };
+
+    const result = makeProfile(user);
+
+    assert.htmlEqual(result, expected);
+});
+
 test('make header template', function(assert) {
     const expected = `
         <header>
