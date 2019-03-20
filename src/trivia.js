bff2f06fbc4db005f6343e52eca8d344ba4c19db
Cosmo Kent <cosmolightfoot@gmail.com>
2019-03-20 11:49:21 -0700

    refactor and style

--- src/trivia.js
+++ src/trivia.js
@@ -28,53 +28,32 @@ fetch(urlRandom)
     .then(fetchedQuestions => {
         const randomQuestions = filterQuestions(fetchedQuestions);
         let question = populateQuestion(randomQuestions, currentQuestionNumber);
+        console.log(question.answer);
+        
         submitButton.addEventListener('click', () => {
             resultBox.classList.remove('hidden');
-            let adjustedCorrectAnswer = question.answer.toUpperCase();
-            adjustedCorrectAnswer = removeCharacters(adjustedCorrectAnswer);
-            let adjustedAnswer = userInput.value.toUpperCase();
-            adjustedAnswer = removeCharacters(adjustedAnswer);
-            resultExpectedAnswer.textContent = adjustedCorrectAnswer;
-            resultUserAnswer.textContent = adjustedAnswer;
-            if(adjustedAnswer === ''){
-                resultCondition.textContent = 'NOT AN ANSWER, TRY ACTUALLY ANSWERING THE QUESTION...';
-                scoreTotal.textContent = score;
-                failureNumber++;
-            }
-            else if(adjustedAnswer.length < 0.9 * adjustedCorrectAnswer.length) {
-                resultCondition.textContent = 'INCORRECT';
-                scoreTotal.textContent = score;
-                failureNumber++;
-            }
-            else if(adjustedCorrectAnswer.includes(adjustedAnswer)){
-                resultCondition.textContent = 'CORRECT';
-                score += question.score;
-                scoreTotal.textContent = score;
-            }
-            else {
-                resultCondition.textContent = 'INCORRECT';
-                scoreTotal.textContent = score;
-                failureNumber++;
-            }
+            resultBox.classList.add('visible');
+            const adjustedAnswer = adjustAnswer();
+            const adjustedCorrectAnswer = adjustCorrectAnswer(question);
+            evaluateAnswer(adjustedAnswer, adjustedCorrectAnswer, question);
             userInput.value = '';
+            
             setTimeout(() => {
                 resultBox.classList.add('hidden');
+                resultBox.classList.remove('visible');
             }, 4000);
-
+            
             if(failureNumber >= 3) {
                 auth.onAuthStateChanged(user => {
-
                     const targetScores = scoresRef.child(user.uid);
                     targetScores.once('value').then(snapshot => {
                         const value = snapshot.val();
-
                         if(!value){
                             scoresRef.child(user.uid)
                                 .set({
                                     highScore: score,
                                     name: user.displayName
-                                });
-                            
+                                });  
                         }
                         else if(score > value.highScore) {
                             scoresRef.child(user.uid)
@@ -84,24 +63,59 @@ fetch(urlRandom)
                                 });  
                         }
                     });
-                  
-                    // window.location = './results.html';
+                    // window.location = './scoreboard.html';
                 });
                 
             }
             
             currentQuestionNumber++;
             question = populateQuestion(randomQuestions, currentQuestionNumber);
+            console.log(question.answer);
         });
     });
-
+    
 function populateQuestion(randomQuestions, currentQuestionNumber) {
     currentQuestionNumberDisplay.textContent = currentQuestionNumber + 1;
     const question = getQuestionFromArray(randomQuestions, currentQuestionNumber);
-    console.log(question);
     questionDisplay.textContent = question.question;
     valueDisplay.textContent = question.score;
     categoryDisplay.textContent = question.category;
     answerDisplay.textContent = question.answer;
     return question;
+}
+
+function adjustCorrectAnswer(question) {
+    let adjustedCorrectAnswer = question.answer.toUpperCase();
+    adjustedCorrectAnswer = removeCharacters(adjustedCorrectAnswer);
+    resultExpectedAnswer.textContent = adjustedCorrectAnswer;
+    return adjustedCorrectAnswer;
+}
+function adjustAnswer() {
+    let adjustedAnswer = userInput.value.toUpperCase();
+    adjustedAnswer = removeCharacters(adjustedAnswer);
+    resultUserAnswer.textContent = adjustedAnswer;
+    return adjustedAnswer;
+}
+
+function evaluateAnswer(adjustedAnswer, adjustedCorrectAnswer, question){
+    if(adjustedAnswer === ''){
+        resultCondition.textContent = 'NOT AN ANSWER, TRY ACTUALLY ANSWERING THE QUESTION...';
+        scoreTotal.textContent = score;
+        failureNumber++;
+    }
+    else if(adjustedAnswer.length < 0.9 * adjustedCorrectAnswer.length) {
+        resultCondition.textContent = 'INCORRECT';
+        scoreTotal.textContent = score;
+        failureNumber++;
+    }
+    else if(adjustedCorrectAnswer.includes(adjustedAnswer)){
+        resultCondition.textContent = 'CORRECT';
+        score += question.score;
+        scoreTotal.textContent = score;
+    }
+    else {
+        resultCondition.textContent = 'INCORRECT';
+        scoreTotal.textContent = score;
+        failureNumber++;
+    }
 }
